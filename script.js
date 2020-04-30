ab5f48760345839cad8551ab9ef5f398e7444c74
Linnea <linneagear@gmail.com>
2020-04-30 16:43:06 -0700

    local storage set item successful, but not getItem

--- script.js
+++ script.js
@@ -6,26 +6,27 @@ displayMoment.innerHTML = nowMoment.format('MMMM Do YYYY, h:mm a');
 // JavaScript function that wraps everything
 $(document).ready(function() {
 
+    // set item before get item
+    $("#save1").on("click", function () {
+        var text = $("#event1").val();
+        console.log(text);
+        //set items in local storage.
+        localStorage.setItem("text", text);
+        console.log(localStorage.getItem("text"));
+    })
+    
 
-$(".saveBtn").on("click", function () {
-    var text = $(".description").val();
-    var time = parseInt($(".hour").text());
 
-    //set items in local storage.
-    localStorage.setItem("time", JSON.stringify(time));
-    localStorage.setItem("text", JSON.stringify(text));
-})
-// need current Time
-// var currentTime = moment().hour();
-var currentTime = 11;
+    // need current Time
+    // var currentTime = moment().hour();
+    var currentTime = 11;
 
     // loop through all time blocks using the each function
     $(".time-block").each(function () {
-        // need the attribute of the ID 
-        var blockHour = parseInt($(this).text());
-        console.log( blockHour, currentTime)
-       
 
+        var blockHour = parseInt($(this).text());
+            console.log( blockHour, currentTime)
+        
         // This will give us the gray, green, and red boxes
         if(blockHour < currentTime) {
             $(this).addClass("past");
@@ -42,46 +43,34 @@ var currentTime = 11;
             $(this).removeClass("past");
             $(this).addClass("future");
         }
+    })
+
+    // function hourNumberFromHourString() {
+    //       "8 AM" = 8
+    //       "9 AM" = 9
+
+        
+    //   }
 
 });
+// data attribute?
 
 
 
-// for every event, save that text to the right time storage
-
-// when time is after current time, then add class "past"
-// when time is before current time, then add class "future"
-// if current time === event time, then add class "present"
-
- //set items in local storage.
-//  localStorage.setItem(time, text);
-});
-
-initializeCalendar();
-
-function initializeCalendar() {
-    // first get local storage from each hour and event to see if anything is saved
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
-    $("#hour6 .description").val(localStorage.getItem("hour6"));
-    $("#hour7 .description").val(localStorage.getItem("hour7"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-
+function newCalendar() {
+    $(".description").empty();
 }
 
-// grab the description class, as this is where our text will be
-// var events = $("description");
-// var event1 = $("#event1");
+function initiliazeCalendar() {
+    
+}
 
-// function storeEvents() {
-//     // Stringify and set "todos" key in localStorage to todos array
-//     localStorage.setItem("events", JSON.stringify(events));
-//   }
 
-//   // store events for that time when click the save button
-//   $("#save1").on("click", function() {
-//     localStorage.setItem("event1", JSON.stringify("#event1"));
+
+function getItems() {
+    // value of ALL events, get the item from ALL hours
+    $("#event").val(localStorage.getItem("hour"));
+}
+
+
+    
